babfc368e61de1150dd0be7d5699f32e7f698689
jsantos93 <jcas@cesar.school>
2020-10-05 20:00:15 -0300

    add lolzinho bot v01

--- config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    DISCORD_BOT_TOKEN:"",
-    general_channel: "732768478406246514",
-    messageTemplate: "Oi @everyone, **{author}** acabou de publicar um vídeo **{title}**!\n{url}",
-    channel_id: "UC2t5bjwHdUX4vM2g8TRDq5g", // League Of legends Channel
-    watchInterval: 30000
-}
