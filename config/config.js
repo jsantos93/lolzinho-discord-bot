9977a4b6117eb690a2b36ed9fab72263eb8bbdc8
jsantos93 <jcas@cesar.school>
2020-10-05 19:02:52 -0300

    add v.01

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    general_channel: "732768478406246514",
+    messageTemplate: "Oi @everyone, **{author}** acabou de publicar um vídeo **{title}**!\n{url}",
+    channel_id: "UC2t5bjwHdUX4vM2g8TRDq5g", // League Of legends Channel
+    watchInterval: 30000
+}
--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const PREFIX = "-"
+var moment = require('moment');
+const { Client } = require('discord.js');
+const { channel_id, channel } = require('../config/config.js');
+const client = new Client();
+client.db = require("quick.db");
+client.request = new (require("rss-parser"));
+client.config = require("../config/config.js");
+const PREFIX = ";"
 
+client.login(process.env.DISCORD_BOT_TOKEN);
 
-client.once('ready', () => {
+const isValidCommand = (message, cmdName) => message.content.toLowerCase().startsWith(PREFIX + cmdName);
+
+client.on('ready', () => {
     console.log(`${client.user.username} has logged in`);
+    lolTime();
+    handleUploads();
 });
 
 client.on('message', (message) =>{
@@ -15,14 +25,47 @@ client.on('message', (message) =>{
     if(message.content.toLowerCase() === "que horas são?"){
         message.reply(`Hora do Lolzinho!`)
     }
-});
-
-client.on('message', (message)=>{
-    if(message.author.bot === true) return;
-    if(message.content.startsWith(PREFIX)){
-        const [CMD_NAME, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
-        
+    else if(isValidCommand(message, "say")){
+        sendAdmMessage(message.content.substring(5))
     }
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+function lolTime(){
+    setInterval(() => {
+        var hour = moment().hour()
+        var minute = moment().minute()
+
+        if(hour === 19 && minute === 00){
+            // const channel = await client.channels.cache.get('762689749818408973');
+            const channel = client.channels.cache.find(channel => channel.name === 'general')
+            channel.send("𝓗𝓸𝓻𝓪 𝓭𝓸 𝓛𝓸𝓵𝔃𝓲𝓷𝓱𝓸!")
+        }
+    }, 20000)
+}  
+
+function handleUploads() {
+    if (client.db.fetch(`postedVideos`) === null) client.db.set(`postedVideos`, []);
+    setInterval(() => {
+        client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.channel_id}`)
+        .then(data => {
+            if (client.db.fetch(`postedVideos`).includes(data.items[0].link)) return;
+            else {
+                client.db.set(`videoData`, data.items[0]);
+                client.db.push("postedVideos", data.items[0].link);
+                let parsed = client.db.fetch(`videoData`);
+                let channel = client.channels.cache.get(client.config.general_channel);
+                if (!channel) return;
+                let message = client.config.messageTemplate
+                    .replace(/{author}/g, parsed.author)
+                    .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
+                    .replace(/{url}/g, parsed.link);
+                channel.send(message);
+            }
+        });
+    }, client.config.watchInterval);
+}
+
+async function sendAdmMessage(message){
+    let channel =  client.channels.cache.get(client.config.general_channel)
+    channel.send(message)
+}
